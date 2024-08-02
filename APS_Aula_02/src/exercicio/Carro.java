ff46ad0de3a16a84e67547f168f7cad936eb7329
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2024-08-01 22:02:38 -0300

    Add files via upload

--- APS_Aula_02/src/exercicio/Carro.java
+++ APS_Aula_02/src/exercicio/Carro.java
@@ -4,7 +4,7 @@ public class Carro {
 
 	private String cor;
 	public String marca;
-	public String anoFabricacao;
+	public int anoFabricacao;
 	public int quantidadePortas;
 	public String tipoCombustivel;
 	
@@ -21,4 +21,23 @@ public class Carro {
 	public String getMarca() {
 		return this.marca;
 	}
+	public int getAnoFabricacao() {
+		return anoFabricacao;
+	}
+	public void setAnoFabricacao(int anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+	public int getQuantidadePortas() {
+		return quantidadePortas;
+	}
+	public void setQuantidadePortas(int quantidadePortas) {
+		this.quantidadePortas = quantidadePortas;
+	}
+	public String getTipoCombustivel() {
+		return tipoCombustivel;
+	}
+	public void setTipoCombustivel(String tipoCombustivel) {
+		this.tipoCombustivel = tipoCombustivel;
+	}
+	
 }
--- APS_Aula_02/src/exercicio/Principal.java
+++ APS_Aula_02/src/exercicio/Principal.java
@@ -1,16 +1,42 @@
 package exercicio;
+import java.util.Scanner;
 
 public class Principal {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Carro gol = new Carro();
 		
-		gol.setCor("Verde");
-		gol.setMarca("VolksWagem");
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe a cor do Carro: ");
+		gol.setCor(scan.nextLine());
+		
+		System.out.println("Informe a marca do carro:");
+		gol.setMarca(scan.nextLine());
+		
+		System.out.println("Informe o tipo de combústivel do carro:");
+		gol.setTipoCombustivel(scan.nextLine());
+		
+		System.out.println("Informe o ano de fabricação do carro:");
+		gol.setAnoFabricacao(scan.nextInt());
+		
+		System.out.println("Informe a quantidade de portas do carro:");
+		gol.setQuantidadePortas(scan.nextInt());
+		
+		
+		
+		/*
+		gol.setCor("Vermelho");
+		gol.setMarco("VolksWagem")
+		gol.setAnoFabricacao(2024);
+		gol.setQuantidadePortas(4);
+		gol.setTipoCombustivel("Flex");*/
 		
 		System.out.println("Cor: " + gol.getCor());
 		System.out.println("Marca: " + gol.getMarca());
+		System.out.println("Ano Fabricação: " + gol.getAnoFabricacao());
+		System.out.println("Quantidade de Portas: " + gol.getQuantidadePortas());
+		System.out.println("Tipo de Combústivel: " + gol.getTipoCombustivel());
 		
 	}
 
